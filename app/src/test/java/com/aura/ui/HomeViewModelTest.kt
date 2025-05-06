24bccac72b0ae77fa3e2ae7b86102d57c9aea648
Olivier Marteaux <olivier_marteaux@hotmail.com>
2025-05-06 17:51:23 +0200

    HomeViewModel tests

--- app/src/test/java/com/aura/ui/HomeViewModelTest.kt
+++ app/src/test/java/com/aura/ui/HomeViewModelTest.kt
@@ -1,19 +1,29 @@
 package com.aura.ui
 
+import android.util.Log
+import com.aura.data.model.Account
+import com.aura.data.model.ServerConnection
 import com.aura.data.repository.AuraRepository
 import com.aura.data.repository.UserPreferencesRepository
 import com.aura.ui.home.HomeViewModel
 import io.mockk.coEvery
+import io.mockk.every
 import io.mockk.mockk
+import io.mockk.mockkStatic
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
 @OptIn(ExperimentalCoroutinesApi::class)
 class HomeViewModelTest {
@@ -21,6 +31,7 @@ class HomeViewModelTest {
     private lateinit var auraRepository: AuraRepository
     private lateinit var userPreferencesRepository: UserPreferencesRepository
     private val testDispatcher = StandardTestDispatcher()
+    private lateinit var accounts: List<Account>
 
     @Before
     fun setup() {
@@ -28,6 +39,11 @@ class HomeViewModelTest {
         userPreferencesRepository = mockk()
         // Mock stored identifier (flowOf emits one value immediately)
         coEvery { userPreferencesRepository.userInput } returns flowOf("initialIdentifier")
+        // Mock server response
+        coEvery { auraRepository.getAccounts(any()) } returns ServerConnection.Loading
+        // Mock Log.d calls
+        mockkStatic(Log::class) // enables mocking static methods like Log.d
+        every { Log.d(any(), any()) } returns 0
         // 🔧 redirects Dispatchers.Main
         Dispatchers.setMain(testDispatcher)
         viewModel = HomeViewModel(auraRepository, userPreferencesRepository)
@@ -39,21 +55,66 @@ class HomeViewModelTest {
     }
 
     @Test
-    fun homeViewModel_Initialization_ExpectedInitialState() {
+    fun homeViewModel_Initialization_ExpectedLoadingState() = runTest { // runTest:coroutine test
+//        advanceUntilIdle() // Wait for the coroutine to complete
         with (viewModel.uiState.value) {
-
-        // user login fetched from UserPreferences Repository
-        assert(viewModel.uiState.value.identifier == "initialIdentifier")
-        // accounts empty
-        assert(viewModel.uiState.value.accounts.isEmpty())
-        // loading state
-//        assertTrue(viewModel.uiState.value.isLoading)
-        // Error state is null
-        assert(viewModel.uiState.value.isError == null)
-        // Balance is 0.0
-        assert(viewModel.uiState.value.balance == 0.0)
+            // Assert user login fetched from UserPreferences Repository
+            assertEquals("initialIdentifier",identifier)
+            // Assert accounts empty
+            assertTrue(accounts.isEmpty())
+            // Assert loading state is enabled
+            assertTrue(isLoading)
+            // Assert Error state is null
+            assertTrue(isError == null)
+            // Assert initial balance is 0.0
+            assertEquals(0.0, balance)
         }
     }
 
+    @Test
+    fun homeViewModel_OnSuccessfulServerConnection_ExpectedSuccessState() = runTest{
+        // Mock successful server response
+        val accounts = listOf(Account("1", true, 100.0), Account("2", false, 200.0))
+        coEvery { auraRepository.getAccounts(any()) } returns ServerConnection.Success(accounts)
+        viewModel.getAccounts()
+        advanceUntilIdle() // Wait for the coroutine to complete
 
+        with (viewModel.uiState.value) {
+            // Assert user login correctly fetched from UserPreferences Repository
+            assertEquals("initialIdentifier",identifier)
+            // Assert accounts fetched from server
+            assertEquals(accounts, this.accounts)
+            // Assert loading state is disabled
+            assertFalse(isLoading)
+            // Assert error state is null
+            assertTrue(isError == null)
+            // Assert balance is correct
+            assertEquals(300.0, balance)
+        }
+    }
+
+    @Test
+    fun homeViewModel_OnServerError_ExpectedErrorState() = runTest {
+        // Mock server response
+        val errorMessage = "Server error"
+        coEvery { auraRepository.getAccounts(any()) } returns ServerConnection.Error(
+            Exception(
+                errorMessage
+            )
+        )
+        viewModel.getAccounts()
+        advanceUntilIdle() // Wait for the coroutine to complete
+        with(viewModel.uiState.value) {
+            // Assert user login correctly fetched from UserPreferences Repository
+            assertEquals("initialIdentifier", identifier)
+            // Assert accounts empty
+            assertTrue(accounts.isEmpty())
+            // Assert loading state is disabled
+            assertFalse(isLoading)
+            // Assert error state is not null
+            assertEquals(errorMessage, isError)
+            // Assert initial balance is 0.0
+            assertEquals(0.0, balance)
+        }
+    }
 }
